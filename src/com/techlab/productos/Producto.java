43cbdeebb427ed16cc0f19f35d75ac5d9849ede5
CristianBrz <cristian.brz.83@gmail.com>
2025-10-21 12:13:28 -0300

    Agrego clase Producto

---
+++ src/com/techlab/productos/Producto.java
@@ -0,0 +1,75 @@
+package com.techlab.productos;
+
+public class Producto {
+
+	private static int proximoId = 0;
+	private int id;
+	private String nombre;
+	private double precio;
+	private int stock;
+
+	public Producto(String nombre, double precio, int stock) {
+		this.id = asignarId();
+		setNombre(nombre);
+		setPrecio(precio);
+		setStock(stock);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		validarNombre(nombre);
+		this.nombre = nombre;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		validarPrecio(precio);
+		this.precio = precio;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		validarStock(stock);
+		this.stock = stock;
+	}
+
+	public String mostarInfo() {
+		return String.format("%d - %s - %.2f - stock: %d", id, nombre, precio, stock);
+	}
+
+	private int asignarId() {
+		return proximoId++;
+	}
+
+	private void validarNombre(String nombre) {
+		if (nombre == null || nombre.trim().isEmpty()) {
+			throw new IllegalArgumentException("Nombre inválido");
+		}
+	}
+
+	private void validarPrecio(double precio) {
+		if (precio < 0) {
+			throw new IllegalArgumentException("El precio no puede ser negativo");
+		}
+	}
+
+	private void validarStock(int stock) {
+		if (stock < 0) {
+			throw new IllegalArgumentException("El stock no puede ser negativo");
+		}
+	}
+
+}
