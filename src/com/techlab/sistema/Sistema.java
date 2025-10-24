226ff713c5477bc3266941dbe3952c8266245560
CristianBrz <cristian.brz.83@gmail.com>
2025-10-23 21:22:49 -0300

    Agrego metodo para buscar porductos en Sistema

--- src/com/techlab/sistema/Sistema.java
+++ src/com/techlab/sistema/Sistema.java
@@ -1,7 +1,9 @@
 package com.techlab.sistema;
 
 import com.techlab.productos.Producto;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -10,7 +12,7 @@ public class Sistema {
 	private Map<Integer, Producto> productos;
 	private final Scanner scanner = new Scanner(System.in);
 	private final String[] OPCIONES_MENU_PRINCIPAL = {
-		"SALIR",
+		"Salir",
 		"Agregar producto",
 		"Listar productos",
 		"Buscar producto",
@@ -76,14 +78,38 @@ public class Sistema {
 
 	private void ejecutarOpcion(int opcion) {
 		switch (opcion) {
-			case 1 -> agregarPorducto(productos);
-			case 2 -> listarProductos();
+			case 1 -> agregarProducto(productos);
+			case 2 -> listarProductos(productos);
+			case 3 -> buscarProductosPorNombre(productos);
 
 			default -> System.out.println("Opción no válida. Intente nuevamente.");
 		}
 	}
 
-	private void agregarPorducto(Map<Integer, Producto> productos) {
+	private void buscarProductosPorNombre(Map<Integer, Producto> productos) {
+		List<Producto> listaProductos = new ArrayList<>(productos.values());
+		buscarProductosPorNombre(listaProductos);
+
+	}
+
+	private void buscarProductosPorNombre(List<Producto> productos) {
+		System.out.println("Ingrese un nombre de un producto: ");
+		String productoBuscado = leerIngresoTeclado().toLowerCase();
+		List<Producto> listaProductosEncontrados = new ArrayList<>();
+
+		for (Producto producto : productos) {
+			String nombreProducto = producto.getNombre().toLowerCase();
+
+			if (nombreProducto.contains(productoBuscado)) {
+				listaProductosEncontrados.add(producto);
+			}
+		}
+
+		listarProductos(listaProductosEncontrados);
+	}
+
+
+	private void agregarProducto(Map<Integer, Producto> productos) {
 		System.out.println("\nAgregando producto...");
 
 		System.out.print("Ingrese el nombre del producto: ");
@@ -111,16 +137,18 @@ public class Sistema {
 			"Smartphone Samsung Galaxy S24",
 			"Tablet Apple iPad Air",
 			"Impresora HP LaserJet",
+			"Impresora HP InkJet",
 			"Disco SSD Kingston 1TB",
+			"Disco SSD Seagate 1TB",
 			"Placa de video NVIDIA RTX 4070"
 		};
 
 		double[] precios = {
 			950000, 85000, 40000, 250000, 300000,
-			1200000, 850000, 200000, 150000, 1800000
+			1200000, 850000, 200000, 250000, 150000, 200000, 1800000
 		};
 
-		int[] stocks = {10, 25, 40, 12, 8, 15, 9, 6, 30, 4};
+		int[] stocks = {10, 25, 40, 12, 8, 15, 9, 10, 6, 30, 40, 4};
 
 		for (int i = 0; i < nombres.length; i++) {
 			Producto producto = new Producto(nombres[i], precios[i], stocks[i]);
@@ -129,8 +157,13 @@ public class Sistema {
 
 	}
 
-	private void listarProductos() {
-		if (productos.isEmpty()) {
+	private void listarProductos(Map<Integer, Producto> productos) {
+		List<Producto> listaProductos = new ArrayList<>(productos.values());
+		listarProductos(listaProductos);
+	}
+
+	private void listarProductos(List<Producto> listaProductos) {
+		if (listaProductos.isEmpty()) {
 			System.out.println("\nNo hay productos en el inventario.\n");
 			return;
 		}
@@ -139,7 +172,7 @@ public class Sistema {
 		System.out.printf(" %2s | %-30s | %-13s | %-5s%n", "ID", "Nombre", "Precio", "Stock");
 		System.out.println("------------------------------------------------------------");
 
-		for (Producto p : productos.values()) {
+		for (Producto p : listaProductos) {
 			System.out.println(p);
 		}
 
